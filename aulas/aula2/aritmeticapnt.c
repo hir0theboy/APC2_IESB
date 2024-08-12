6d5cc18a63b43d63639b5772b9096fe466b2bfd5
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-08-12 12:56:45 +0000

    aula2 1.1

--- aulas/aula2/aritmeticapnt.c
+++ aulas/aula2/aritmeticapnt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-printf("sizeof(int) = %d\n", sizeof(int));
-printf("sizeof(float) = %d\n", sizeof(float));
-printf("sizeof(double) = %d\n", sizeof(double));
-printf("sizeof(char) = %d\n", sizeof(char));
+printf("sizeof(int) = %d\n", sizeof(int)); //inteiro ocupa 4 bytes, endereço vai de 4 em 4
+printf("sizeof(float) = %d\n", sizeof(float)); //float ocupa 4 bytes, endereço vai de 4 em 4
+printf("sizeof(double) = %d\n", sizeof(double)); //double ocupa 8 bytes, endereço vai de 8 em 8
+printf("sizeof(char) = %d\n", sizeof(char)); //char ocupa 1 bytes, endereço vai de 1 em 1
 
   
 int inteiro = 100; //&inteiro = 0x100
@@ -13,11 +13,19 @@ int inteiro = 100; //&inteiro = 0x100
 
 int *pint = &inteiro;
   printf("pint: %ul, pint+1: %ul, pint+20: %ul\n", pint, pint+1, pint+20); //como o inteiro ocupa 4 bytes, a soma nao vai de 1 em 1, vai de 4 em 4
-  printf("pint: %ul, pint-1: %ul, pinto-20: %ul\n", pint, pint-1, pint-20);
+  printf("pint: %ul, pint-1: %ul, pint-20: %ul\n", pint, pint-1, pint-20);
+
+  float *pfloat = (float *)&inteiro; 
+  printf("pfloat: %ul, pfloat+1: %ul, pfloat+20: %ul\n", pfloat, pfloat+1, pfloat+20);
+  printf("pfloat: %ul, pfloat-1: %ul, pfloat-20: %ul\n", pfloat, pfloat-1, pfloat-20);
+
+  char *pdouble = (void *)&inteiro; 
+  printf("pdouble: %ul, pdouble+1: %ul, pdouble+20: %ul\n", pdouble, pdouble+1, pdouble+20);
+  printf("pdouble: %ul, pdouble-1: %ul, pdouble-20: %ul\n", pdouble, pdouble-1, pdouble-20);
 
   char *pchar = (char *)&inteiro; 
   printf("pchar: %ul, pchar+1: %ul, pchar+20: %ul\n", pchar, pchar+1, pchar+20);
-  printf("pchar: %ul, pchar-1: %ul, pcharo-20: %ul\n", pchar, pchar-1, pchar-20);
+  printf("pchar: %ul, pchar-1: %ul, pchar-20: %ul\n", pchar, pchar-1, pchar-20);
   
   return 0;
 }
