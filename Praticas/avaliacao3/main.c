85b5451cfcfeeda30228ef8f0b13b1d2041a98f6
Felipe Alves <132400951+hir0theboy@users.noreply.github.com>
2024-09-02 13:55:58 +0000

    atividade3.1

--- Praticas/avaliacao3/main.c
+++ Praticas/avaliacao3/main.c
@@ -58,7 +58,7 @@ int main() {
   int imagem4[10][10];
   for (int x = 0; x < 10; x++) {
     for (int y = 0; y < 10; y++) 
-      imagem4[x][y] = imagem[x][y]; // Branco
+      imagem4[x][y] = imagem[x][y]; 
   }
 
   // Exercício 1
